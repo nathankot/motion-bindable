c8ee43e499372f96b4fc75a21a9e2cce398a96ec
Nathan Kot <me@nathankot.com>
2013-12-14 03:12:50 +1300

    Formatting.

--- lib/motion_bindable/bindable.rb
+++ lib/motion_bindable/bindable.rb
@@ -48,11 +48,11 @@ module MotionBindable
     end
 
     def underscore(str)
-      str.gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("-", "_").
-      downcase
+      str.gsub(/::/, '/')
+      .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+      .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+      .tr("-", "_")
+      .downcase
     end
 
   end
