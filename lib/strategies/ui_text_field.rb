180392bbc9754c3c235dfce3d7a53b88768e1820
Nathan Kot <me@nathankot.com>
2013-12-24 00:07:58 +1300

    UITextfield using the new strategy interface.

--- lib/strategies/ui_text_field.rb
+++ lib/strategies/ui_text_field.rb
@@ -4,36 +4,33 @@ module MotionBindable::Strategies
 
     include BW::KVO
 
-    def on_bind
-      refresh
-
-      # Observe the bound object
-      NSNotificationCenter.defaultCenter.addObserver(
-        self,
-        selector: :on_bound_change,
-        name: UITextFieldTextDidChangeNotification,
-        object: bound
+    def start_observing_bound
+      NSNotificationCenter.defaultCenter.addObserverForName(
+        UITextFieldTextDidChangeNotification,
+        object: bound,
+        queue: nil,
+        usingBlock: proc { |_| on_bound_change }
       )
+    end
 
+    def start_observing_object
       # Observe the attribute
       observe(object, @attr_name) { |_, _| on_object_change }
     end
 
-    def on_bound_change
-      self.attribute = bound.text unless bound.text.empty?
+    def on_bound_change(new = nil)
+      self.attribute = new || bound.text
     end
 
-    def on_object_change
-      @bound.text = attribute
+    def on_object_change(new = nil)
+      @bound.text = new || attribute
     end
 
     def unbind
       App.notification_center.unobserve(@bound_observer)
       unobserve(object, @attr_name)
+      super
     end
-
-    # Text field takes precedence
-    alias_method :refresh, :on_bound_change
 
   end
 
